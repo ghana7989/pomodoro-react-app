99468ffdfd7080ec29cff972aab79bc95d5df722
ghana7989 <ghana.chindukuri@gmail.com>
2020-09-13 10:24:30 +0530

    Updates

---
+++ precache-manifest.fbcbeb96da40652d787afbbfb352105c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc70f02428cf88ec4564132c02cfabe6",
+    "url": "/pomodoro-react-app/index.html"
+  },
+  {
+    "revision": "3601c8dafda0959397f6",
+    "url": "/pomodoro-react-app/static/css/main.540a1c28.chunk.css"
+  },
+  {
+    "revision": "33ec6890519b71d05645",
+    "url": "/pomodoro-react-app/static/js/2.b6dfed0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-react-app/static/js/2.b6dfed0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3601c8dafda0959397f6",
+    "url": "/pomodoro-react-app/static/js/main.10a746f3.chunk.js"
+  },
+  {
+    "revision": "def75643df9b08181b37",
+    "url": "/pomodoro-react-app/static/js/runtime-main.2dd61b47.js"
+  }
+]);
